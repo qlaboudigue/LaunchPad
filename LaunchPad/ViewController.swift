8f8883b23d3cc3c21568eb9a1cb6ba864f30d03f
Laboudigue quentin <qlaboudigue@gmail.com>
2021-06-03 11:39:10 +0200

    First commit

--- LaunchPad/ViewController.swift
+++ LaunchPad/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // Properties
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    @IBOutlet weak var buttonFive: UIButton!
+    @IBOutlet weak var buttonSix: UIButton!
+    @IBOutlet weak var buttonSeven: UIButton!
+    @IBOutlet weak var buttonEight: UIButton!
+    @IBOutlet weak var buttonNine: UIButton!
+    @IBOutlet weak var buttonTen: UIButton!
+    @IBOutlet weak var buttonEleven: UIButton!
+    @IBOutlet weak var buttonTwelve: UIButton!
+    
+    
+    let blueGradient = UIImage(named: "blueGradient")
+    let greenGradient = UIImage(imageLiteralResourceName: "greenGradient")
+    
+    var audioAsset: AVURLAsset!
+    var player: AVAudioPlayer!
+    var timer = Timer()
+    
+    // Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let buttonList = [buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonSeven, buttonEight, buttonNine, buttonTen, buttonEleven, buttonTwelve]
+        
+        for i in 0..<buttonList.count {
+            buttonList[i]?.accessibilityLabel = "\(i+1)"
+            let image = UIImage(named: "\(i+1)")
+            buttonList[i]?.setBackgroundImage(blueGradient, for: .normal)
+            buttonList[i]?.setImage(image, for: .normal)
+            buttonList[i]?.contentMode = .center
+            buttonList[i]?.imageView?.contentMode = .scaleAspectFit
+        }
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if let key = sender.accessibilityLabel {
+            sender.setBackgroundImage(greenGradient, for: .normal)
+            timer = Timer.scheduledTimer(withTimeInterval: timeDuration(key: key), repeats: false) { timer in
+                sender.setBackgroundImage(self.blueGradient, for: .normal)
+            }
+            playSound(key: key)
+        }
+    }
+    
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func timeDuration(key: String) -> Double {
+        if let url = Bundle.main.url(forResource: key, withExtension: "wav") {
+            audioAsset = AVURLAsset(url: url)
+            let duration = audioAsset.duration
+            let durationSeconds = CMTimeGetSeconds(duration)
+            return Double(durationSeconds)
+        } else {
+            return 0.0
+        }
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+# Launchpad
+
+## Core function(s)
+### The goal is to create a single page app with 12 buttons triggering sounds. 
+
+## UI/UX design
+### As a Launchpad, a well known musical physical device, the app must show a set of equally distributed buttons on screen. Each button, filled by a blue gradient, must display an icon as a clue for users to trigger the right sound. Pressed, the button color switches to a green color gradient during the corresponding sound duration. Shapes and icons, must adapt to multiple devices and orientations.
+
+## User request
+### Tha app has been ordered / requested by a bunch of friends who wanted to animate a gaming night. Logo, backgrounds and colors have been created designed based on the played game theme. Sounds have been provided by the users. 
